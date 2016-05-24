1eeabde70d7c9b4a2ca2009106766822bc053b8f
Ranvijay <root@ranvijayj.com>
2016-05-24 14:57:11 +0530

    Added cookbook elasticsearch

---
+++ cookbook-elasticsearch/metadata.rb
@@ -0,0 +1,13 @@
+# Encoding: utf-8
+name             'elasticsearch'
+maintainer       'Karel Minarik'
+maintainer_email 'karel.minarik@elasticsearch.org'
+license          'Apache 2.0'
+description      'Installs and configures Elasticsearch'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '2.3.1'
+
+depends 'apt'
+depends 'yum'
+depends 'chef-sugar'
+depends 'ark'
