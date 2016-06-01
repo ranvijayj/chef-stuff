5688f4cdb0e615cd7a8972947a2f521c723f05c6
Ranvijay <root@ranvijayj.com>
2016-06-01 11:29:53 +0530

    logstashconfL2

--- logstash/recipes/default.rb
+++ logstash/recipes/default.rb
@@ -2,11 +2,11 @@ execute 'wgetlogstash' do
   command 'wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -'
   user "root"
 end
-
+#line=deb https://packages.elastic.co/logstash/2.3/debian stable main
 #ruby_block "insert_line" do
 #  block do
 #    file = Chef::Util::FileEdit.new("/etc/apt/sources.list")
-#    file.insert_line_if_no_match("deb https://packages.elastic.co/logstash/2.3/debian stable main/", "")
+#    file.insert_line_if_no_match("/#{line}/", "#{line}")
 #    file.write_file
 #  end
 #end
@@ -33,20 +33,6 @@ ruby_block 'set-env-java-home' do
   end
 end
 
-#ruby_block 'set-env-java-home' do
-#  block do
-#    ENV['JAVA_HOME'] = java_home
-#  end
-#end
-#
-#java_home = "export JAVA_HOME=/usr/java/jdk1.6.0_31"
-#path = "export PATH=$PATH:JAVA_HOME/bin"
-#file "/etc/profile" do
-#  content "#{java_home}\n#{path}"
-#  owner "root"
-#end
-
-
 
 execute 'aptupdate' do
   command 'apt-get update'
@@ -60,7 +46,12 @@ end
 
 cookbook_file '/etc/logstash/conf.d/logstash.conf' do
        owner 'root'
-       source 'logstash.conf'
+       source 'logstash.conf.l2'
+end
+
+cookbook_file '/etc/init.d/logstash' do
+       owner 'root'
+       source 'logstashinit'
 end
 
 
@@ -68,7 +59,17 @@ service "logstash" do
   action :stop
 end
 
+#execute 'changepersmissions' do
+#  command 'chmod 644 /var/log/nginx/*'
+#end
+
+#directory '/var/log/nginx/' do
+#  mode '0644'
+#  action :create
+#end
+
+
 execute 'startlogstatsh' do
-  command '/opt/logstash/bin/logstash -f /etc/logstash/conf.d/logstash.conf'
+  command 'service logstash start'
   only_if '/opt/logstash/bin/logstash -f /etc/logstash/conf.d/logstash.conf --configtest'
 end
