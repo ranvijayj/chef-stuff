96a82d63c8b35fe37a7b829eaf8d34e3dd0a8abd
Ranvijay <root@ranvijayj.com>
2016-06-03 17:47:04 +0530

    changes template for 2 hosts in same template

--- nagios/recipes/nagios-newclient.rb
+++ nagios/recipes/nagios-newclient.rb
@@ -4,15 +4,15 @@ directory '/etc/nagios3/conf.d/hosts.d/utility' do
   mode '0755'
 end
 
-template "/etc/nagios3/conf.d/hosts.d/elk/utility.cfg" do
+template "/etc/nagios3/conf.d/hosts.d/utility/utility.cfg" do
   source "tata-hosts.erb"
-  variables( :a => 'jenkins', :b => "30.0.30.71" )
+  variables( :a => 'jenkins', :b => "30.0.30.71", :c => 'openvpnbastion', :d => "30.0.20.169" )
 end
 
-template "/etc/nagios3/conf.d/hosts.d/elk/utility.cfg" do
-  source "tata-hosts.erb"
-  variables( :c => 'openvpnbastion', :d => "30.0.20.169" )
-end
+#template "/etc/nagios3/conf.d/hosts.d/utility/openvpn.cfg" do
+#  source "tata-hosts.erb"
+#  variables( :c => 'openvpnbastion', :d => "30.0.20.169" )
+#end
 
 directory '/etc/nagios3/conf.d/hostgroups.d/utility' do
   action :create
@@ -20,7 +20,7 @@ directory '/etc/nagios3/conf.d/hostgroups.d/utility' do
   mode '0755'
 end
 
-template "/etc/nagios3/conf.d/hostgroups.d/elk/utility.cfg" do
+template "/etc/nagios3/conf.d/hostgroups.d/utility/utility.cfg" do
   source "tata-hostgroups.erb"
   variables( :e => 'utility', :f => 'jenkins,openvpnbastion' )
 end
@@ -31,7 +31,7 @@ directory '/etc/nagios3/conf.d/services.d/utility' do
   mode '0755'
 end
 
-template "/etc/nagios3/conf.d/services.d/elk/utility.cfg" do
+template "/etc/nagios3/conf.d/services.d/utility/utility.cfg" do
   source "tata-services.erb"
   variables( :g => 'utility' )
 end
