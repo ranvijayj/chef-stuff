1c2f99a78699682b42e685330873cb8bed2df6a0
Ranvijay <root@ranvijayj.com>
2016-06-03 14:12:59 +0530

    added nagios recipes and templates to monitor new client

---
+++ nagios/recipes/nagios-newclient.rb
@@ -0,0 +1,40 @@
+directory '/etc/nagios3/conf.d/hosts.d/elk' do
+  action: create
+  owner 'root'
+  mode '0755'
+end
+
+template "/etc/nagios3/conf.d/hosts.d/elk/elk.cfg" do
+  source "tata-hosts.erb"
+  variables( :a => 'elk', :b => '30.0.30.20' )
+end
+
+
+directory '/etc/nagios3/conf.d/hostgroups.d/elk' do
+  action: create
+  owner 'root'
+  mode '0755'
+end
+
+template "/etc/nagios3/conf.d/hostgroups.d/elk/elk.cfg" do
+  source "tata-hosts.erb"
+  variables( :a => 'elk', :b => 'elk' )
+end
+
+directory '/etc/nagios3/conf.d/services.d/elk' do
+  action: create
+  owner 'root'
+  mode '0755'
+end
+
+template "/etc/nagios3/conf.d/services.d/elk/elk.cfg" do
+  source "tata-services.erb"
+  variables( :a => 'elk' )
+end
+
+
+service 'nagios3' do
+      supports :restart => true, :reload => true
+      action :enable
+      subscribes :reload, 'template[/etc/nagios3/conf.d/services.d/elk/elk.cfg]', :immediately
+end
