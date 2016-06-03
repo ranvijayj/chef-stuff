37d6055eb62c73d12e6ad6fe8ca1b14025346d4b
Ranvijay <root@ranvijayj.com>
2016-06-03 17:32:18 +0530

    changes template 2 servers

--- nagios/recipes/nagios-newclient.rb
+++ nagios/recipes/nagios-newclient.rb
@@ -1,35 +1,39 @@
-directory '/etc/nagios3/conf.d/hosts.d/elk' do
+directory '/etc/nagios3/conf.d/hosts.d/utility' do
   action :create
   owner 'root'
   mode '0755'
 end
 
-template "/etc/nagios3/conf.d/hosts.d/elk/elk.cfg" do
+template "/etc/nagios3/conf.d/hosts.d/elk/utility.cfg" do
   source "tata-hosts.erb"
-  variables( :a => 'elk', :b => "30.0.30.20" )
+  variables( :a => 'jenkins', :b => "30.0.30.71" )
 end
 
+template "/etc/nagios3/conf.d/hosts.d/elk/utility.cfg" do
+  source "tata-hosts.erb"
+  variables( :c => 'openvpnbastion', :d => "30.0.20.169" )
+end
 
-directory '/etc/nagios3/conf.d/hostgroups.d/elk' do
+directory '/etc/nagios3/conf.d/hostgroups.d/utility' do
   action :create
   owner 'root'
   mode '0755'
 end
 
-template "/etc/nagios3/conf.d/hostgroups.d/elk/elk.cfg" do
+template "/etc/nagios3/conf.d/hostgroups.d/elk/utility.cfg" do
   source "tata-hostgroups.erb"
-  variables( :a => 'elk', :c => 'elk' )
+  variables( :e => 'utility', :f => 'jenkins,openvpnbastion' )
 end
 
-directory '/etc/nagios3/conf.d/services.d/elk' do
+directory '/etc/nagios3/conf.d/services.d/utility' do
   action :create
   owner 'root'
   mode '0755'
 end
 
-template "/etc/nagios3/conf.d/services.d/elk/elk.cfg" do
+template "/etc/nagios3/conf.d/services.d/elk/utility.cfg" do
   source "tata-services.erb"
-  variables( :a => 'elk' )
+  variables( :g => 'utility' )
 end
 
 
